8551cee96a83362235be93137e8c9391de8ceb40
Ateeth Kosuri <ateethkosuri@wifi-131-179-62-32.host.ucla.edu>
2016-09-28 09:12:46 -0700

    Collection View started.

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Ateeth Kosuri on 9/27/16.
+//  Copyright © 2016 Ateeth Kosuri. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Ateeth Kosuri on 9/27/16.
+//  Copyright © 2016 Ateeth Kosuri. All rights reserved.
+//
+
+import Foundation
